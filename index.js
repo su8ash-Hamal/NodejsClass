4973559d1e848bda35375c1c9854b915a8d2e818
su8ash-Hamal <subash.hamal.dev22@gmail.com>
2024-07-09 10:37:17 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,108 @@
+// s = "jello";
+// console.log(typeof s);
+// console.log(s);
+
+// var name = "Subash";
+// console.log(name);
+
+// let hobbies = "Coding, Nodejs, Reactjs";
+// console.log(hobbies);
+
+// const learning = "Always";
+// console.log(learning);
+// var a = 40;
+// var b = 20;
+
+// function test() {
+//   let a = 20;
+//   b = 30;
+
+//   console.log(a);
+//   console.log(b);
+// }
+
+// test();
+
+// console.log(b);
+// console.log(a);
+
+// prabin2();
+// prabin("Test");
+
+// const prabin = (givenName) => {
+//   console.log("Hello " + givenName);
+//   console.log("Hello ", givenName);
+// };
+
+// function prabin2() {
+//   console.log("Prabin 2 is printing");
+// }
+
+// prabin("Suaper", "Jello");
+
+// function simpletVarAndLetExample() {
+//   if (true) {
+//     var x = 10;
+//     let y = 20;
+//   }
+//   console.log(x); // Output: ?
+//   console.log(y); // Output: ?
+// }
+
+// // simpletVarAndLetExample();
+
+// let student = {
+//   id: "1",
+//   name: "ABC",
+// };
+
+// let arr = ["Hello", 1, "2"];
+
+// console.log(student.name, student.id);
+
+// console.log(arr[2]);
+
+// console.log(typeof arr[1]);
+
+// let newArr = arr.map((item) => {
+//   console.log(item);
+//   return item + " new";
+// });
+
+// console.log(newArr);
+
+// let a = [1, 2, 3, 4];
+
+// let b = [...a];
+
+// console.log(b);
+
+// a.push(5);
+
+// console.log(b);
+
+// let m = "Hello";
+
+// let t = m;
+
+// m = "test";
+
+// console.log(t);
+
+// function restTest(...a) {
+//   console.log([...a]);
+// }
+
+// restTest(10, 20);
+
+let test = {
+  id: "Hello",
+  name: "Jello",
+  age: "trello",
+};
+
+let { name, age } = test;
+
+console.log(test);
+console.log(name);
+console.log(age);
